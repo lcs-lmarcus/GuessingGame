0d4c67666d50c772f54debef42ba4ddcb9a00257
Marcus Li <mmli@lcs.on.ca>
2025-01-16 14:39:24 -0500

    Re-organized project and created a GameView file.

---
+++ Views/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  GuessingGame
+//
+//  Created by Marcus Li on 2025-01-16.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
